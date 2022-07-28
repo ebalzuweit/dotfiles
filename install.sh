21a0378bd4816a4561da035b6f12df4d97fc281b
Evan Balzuweit <ebalzuweit@gmail.com>
2022-07-27 20:35:18 -0500

    add installation script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+# adapted from https://github.com/mathiasbynens/dotfiles/blob/main/bootstrap.sh
+
+function installDotfiles() {
+    echo "Installing:";
+
+    echo "  .bash_profile";
+    cp .bash_profile ~;
+    echo "  .bash_prompt";
+    cp .bash_prompt ~;
+    echo "  .bashrc";
+    cp .bashrc ~;
+    echo "  .vimrc";
+    cp .vimrc ~;
+    echo "  .gitignore - Automatic installation not supported at this time.";
+
+    source ~/.bash_profile;
+}
+
+read -p "This is a one-way, destructive process. Are you sure? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    installDotfiles;
+fi;
+
+unset installDotfiles;
