ee211c9b688812215f2600d6025a055c365702e8
ebalzuweit <ebalzuweit@gmail.com>
2024-03-01 11:43:37 -0500

    Add zsh support

--- install.sh
+++ install.sh
@@ -5,17 +5,26 @@
 function installDotfiles() {
     echo "Installing:";
 
+    # copy files to home
+    echo "  .aliases";
+    cp .aliases ~;
     echo "  .bash_profile";
     cp .bash_profile ~;
     echo "  .bash_prompt";
     cp .bash_prompt ~;
     echo "  .bashrc";
     cp .bashrc ~;
+    echo "  .zshrc";
+    cp .zshrc ~;
     echo "  .vimrc";
     cp .vimrc ~;
     echo "  .gitconfig - Automatic installation not supported at this time.";
 
-    source ~/.bash_profile;
+    echo ""
+    echo "dotfiles have been updated successfully!"
+    echo "Please restart your shell or source the appropriate file:"
+    echo "- ~/.bash_profile"
+    echo "- ~/.zshrc"
 }
 
 read -p "This is a one-way, destructive process. Are you sure? (y/n) " -n 1;
