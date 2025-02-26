ac2a111fe0e99b0c8055a0651fdf8debb73456f0
ebalzuweit <ebalzuweit@gmail.com>
2025-02-26 08:03:47 -0800

    Escape terminal mode with <Esc><Esc>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Escape Terminal mode with <Esc>
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {})
+-- Escape Terminal mode with <Esc><Esc>
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", {})
