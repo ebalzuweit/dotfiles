2210bf4e74efd14485c4164119558a9a75af1bdc
Matthew Myrick <matthewmyrick2@gmail.com>
2025-08-01 09:53:27 -0400

    Update

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = { enabled = true },
+      panel = { enabled = true },
+      filetypes = {
+        ["*"] = true, -- Enable for all file types
+      },
+    },
+  },
+}
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      filters = {
+        dotfiles = false, -- Show dotfiles
+        git_ignored = false, -- Show git ignored files
+        custom = {}, -- No custom filters
+      },
+    },
+  },
+}
